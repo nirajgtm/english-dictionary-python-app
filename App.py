be00b5a9c75fb826edf101a4cd958f1b94ec62b3
Niraj Gautam <nirajgtm@gmail.com>
2019-06-19 19:37:21 -0400

    added close match for wrong word

--- App.py
+++ App.py
@@ -1,11 +1,18 @@
 import	json
+from difflib import get_close_matches #Library to compare text
 data = json.load(open("dictionary-file.json"))
 
 def meaning(word):
 	if word in data:
-		return data[word]
-	else:
-		print("incorrect word bitch")
+		for value in data[word]:
+			 return value
+			 print("\n")
+		
+	elif get_close_matches(word, data.keys(), cutoff=0.6) !=  []:
+		matches = get_close_matches(word, data.keys(), cutoff=0.6)
+		return("Words Does not Exist! \n Closest match is: '{0}'" .format(matches[0]))
+	else :
+		return("Please check your word, it doesn't make any sense")
 
 word = input ("Enter the word: ")
 
